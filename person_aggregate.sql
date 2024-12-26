bf1824758e7abaf705a87dbee919b94324079496
vanbaomad <140963493+vanbaomad@users.noreply.github.com>
2024-12-26 15:08:01 +0700

    aggregate function

--- person_aggregate.sql
+++ person_aggregate.sql
@@ -11,7 +11,6 @@ WHERE address LIKE '1%y';
 SELECT address
 FROM address
 WHERE SUBSTR(address, 3, 1) = 'a';
-use sakila;
 -- cau 4
 SELECT first_name, last_name
 FROM customer
@@ -25,28 +24,20 @@ INNER JOIN category ON film_category.category_id = category.category_id
 WHERE category.name = 'Games';
 -- cau 6
 SELECT *
-FROM person
+FROM Person
 WHERE salary BETWEEN 2000 AND 3000;
 -- cau 7
-SELECT a.actor_id, a.first_name, a.last_name, COUNT(fa.film_id) AS film_count
-FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
-GROUP BY a.actor_id, a.first_name, a.last_name
-HAVING film_count > 30
-ORDER BY film_count DESC;
+SELECT *
+FROM Person
+WHERE MONTH(birthday) BETWEEN 7 AND 12;
 -- cau 8
-SELECT a.actor_id, a.first_name, a.last_name
-FROM actor a
-LEFT JOIN film_actor fa ON a.actor_id = fa.actor_id
-WHERE fa.actor_id IS NULL;
+SELECT *, 'Happy birthday!' AS message
+FROM Person
+WHERE MONTH(birthday) = MONTH(CURDATE());
 -- cau 9
-SELECT f.title, SUM(p.amount) AS total_revenue
-FROM film f
-JOIN inventory i ON f.film_id = i.film_id
-JOIN rental r ON i.inventory_id = r.inventory_id
-JOIN payment p ON r.rental_id = p.rental_id
-GROUP BY f.film_id, f.title
-ORDER BY total_revenue DESC;
+SELECT *
+FROM Person
+WHERE YEAR(birthday) IN (2000, 1990, 1991);
 
 
 
